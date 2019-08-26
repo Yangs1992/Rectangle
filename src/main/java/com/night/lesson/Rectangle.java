c301dd81997c62215735e3cd46c3e307a8c91690
yangsen <y7ngyang@qq.com>
2019-08-26 20:03:11 +0800

    lesson task rectangle

---
+++ src/main/java/com/night/lesson/Rectangle.java
@@ -0,0 +1,38 @@
+package com.night.lesson;
+
+public class Rectangle {
+    private int width;
+
+    private int length;
+
+
+    public Rectangle(int width, int length) {
+        this.width = width;
+        this.length = length;
+    }
+
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getArae() {
+        return width * length;
+    }
+
+    public int getPerimeter(){
+        return (width + length) * 2;
+    }
+}
